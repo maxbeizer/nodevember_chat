a96c0652ba0dc63f57acb41697b3f8f9929f5dcc
Max Beizer <max.beizer@gmail.com>
2015-11-14 17:51:46 -0600

    Add HOST env var to config/prod

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :nodevember_chat, NodevemberChat.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "nodevemberchat.herokuapp.com", port: 443],
+  url: [host: System.get_env("HOST"), port: 443],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
